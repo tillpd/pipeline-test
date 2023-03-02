eec6d7644e47a599c45ee59f5c97067cda0dc765
tillpd <till.wierbach@tierarztpluspartner.de>
2023-03-02 14:56:49 +0100

    add db

--- apps/web/index.js
+++ apps/web/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const db = require('./db')
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
